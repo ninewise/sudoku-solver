ea43405952f58413cb2b7d2c39cc93ef12798916
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2013-03-07 17:52:36 +0100

    Add comments and clean up

--- sudoku-solver.hs
+++ sudoku-solver.hs
@@ -7,6 +7,8 @@ import qualified Data.Map      as M
 import qualified Data.Foldable as F
 
 --------------------------------------------------------------------------------
+-- Cell represents a cell in the sudoku grid. Contains a possibility of digits,
+-- a certain digit or is invalid.
 data Cell = E [Int] | D Int | X
     deriving (Eq)
 
@@ -15,13 +17,17 @@ instance Show Cell where
     show (D n)  = show n
 
 --------------------------------------------------------------------------------
+-- Tile is the location of a cell in the sudoku grid.
 data Tile = Tile Int Int
     deriving (Eq, Show, Ord)
 
 --------------------------------------------------------------------------------
+-- A sudoku maps the 81 tiles on their cells.
 type Sudoku = Map Tile Cell
 
 --------------------------------------------------------------------------------
+-- Read a sudoku from stdin. Format: 9 lines of 9 characters each, where a digit
+-- is parsed as that digit, and any other character as an empty cell.
 parseSudoku :: IO Sudoku
 parseSudoku = do
     lines <- replicateM 9 getLine
@@ -34,6 +40,8 @@ parseSudoku = do
                    | otherwise      = E [1..9]
 
 --------------------------------------------------------------------------------
+-- fixTile assumes the given tile is correct, and removes all clashing
+-- possibilities. If there's a clash with a certain digit, Nothing is returned.
 fixTile :: Tile -> Sudoku -> Maybe Sudoku
 fixTile (Tile r c) s = validate $ mapWithKey (filterCell $ s M.! Tile r c) s
   where
@@ -58,10 +66,14 @@ fixTile (Tile r c) s = validate $ mapWithKey (filterCell $ s M.! Tile r c) s
     validate s = if F.any (== X) s then Nothing else Just s
 
 --------------------------------------------------------------------------------
+-- Calls fixTile for every tile in the Sudoku. This method leaves only valid
+-- possibilities, and returns Nothing if there are clashing values in the
+-- Sudoku.
 iterateFixes :: Sudoku -> Maybe Sudoku
 iterateFixes sudoku = F.foldrM fixTile sudoku (keys sudoku)
 
 --------------------------------------------------------------------------------
+-- Makes all tiles where there is only one possible digit left fixed.
 fixSingles :: Sudoku -> Sudoku
 fixSingles = M.map go
   where
@@ -69,6 +81,8 @@ fixSingles = M.map go
     go x       = x
 
 --------------------------------------------------------------------------------
+-- Repeates removing possibilities and fixing cells with only one possible digit
+-- until no further change is made this way.
 fillSudoku :: Sudoku -> Sudoku
 fillSudoku sudoku = fst $ until noChange nextIteration (empty, sudoku)
   where
@@ -76,26 +90,32 @@ fillSudoku sudoku = fst $ until noChange nextIteration (empty, sudoku)
     nextIteration (_, old) = (old, fixSingles $ fromJust $ iterateFixes old)
 
 --------------------------------------------------------------------------------
-solveSudoku :: Tile -> Maybe Sudoku -> Maybe Sudoku
-solveSudoku _ Nothing = Nothing
-solveSudoku (Tile r c) (Just s)
-  | c >= 9      = solveSudoku (Tile (r + 1) 0) $ Just s
-  | r >= 9      = Just s
-  | otherwise   = case s M.! (Tile r c) of
-        E list -> let
-                    tile = Tile r c
-                    test n = solveSudoku (Tile r $ c + 1) $
-                                fixTile tile $ M.insert tile (D n) s
-                  in msum $ map test list
-        D n -> solveSudoku (Tile r $ c + 1) $ Just s
+-- Solves the sudoku, but returns Nothing if the sudoku is invalid.
+solveSudoku :: Sudoku -> Maybe Sudoku
+solveSudoku = go (Tile 0 0) . return
+  where
+    go :: Tile -> Maybe Sudoku -> Maybe Sudoku
+    go _ Nothing = Nothing
+    go (Tile r c) (Just s)
+      | c >= 9      = go (Tile (r + 1) 0) $ Just s
+      | r >= 9      = Just s
+      | otherwise   = case s M.! (Tile r c) of
+            E list -> let
+                        tile = Tile r c
+                        test n = go (Tile r $ c + 1) $
+                                    fixTile tile $ M.insert tile (D n) s
+                      in msum $ map test list
+            D n -> go (Tile r $ c + 1) $ Just s
 
 --------------------------------------------------------------------------------
+-- The main method. Reads a number of sudoku's from stdin and prints their
+-- solutions.
 main :: IO ()
 main = do
     times <- (readLn :: IO Int)
     replicateM_ times $ do 
         sudoku <- parseSudoku
-        print $ solveSudoku (Tile 0 0) (Just sudoku)
+        print $ solveSudoku sudoku
         _ <- getLine
         putStrLn ""
     
